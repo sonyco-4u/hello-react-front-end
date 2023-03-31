7de91212a4970c00c25da1f3290395b4ef3a57e8
Ezema Anthony Sunday <tonyunday@gmail.com>
2023-03-31 03:18:18 -0700

    Created greeting component, store and greetingSlice

---
+++ src/components/Greeting.jsx
@@ -0,0 +1,22 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getGreetings } from '../redux/greetings/greetingSlice';
+
+const Greetings = () => {
+  const dispatch = useDispatch();
+  const { greeting, error } = useSelector((state) => state.greetings);
+
+  useEffect(() => {
+    dispatch(getGreetings());
+  }, []);
+
+  if (error) {
+    return <p>{error}</p>;
+  }
+
+  return (
+    <p className="message">{greeting.message}</p>
+  );
+};
+
+export default Greetings;
---
+++ src/redux/greetings/greetingSlice.js
@@ -0,0 +1,31 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+export const getGreetings = createAsyncThunk('greetings/fetch', async () => {
+  const greetings = await fetch('http://localhost:4000/api/greetings');
+  const data = await greetings.json();
+  return data;
+});
+
+const initialState = {
+  greeting: '',
+  error: '',
+};
+
+const greetingSlice = createSlice({
+  name: 'greetings',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(getGreetings.fulfilled, (state, action) => {
+        const newState = { ...state, greeting: action.payload, loading: false };
+        return newState;
+      })
+      .addCase(getGreetings.rejected, (state) => {
+        const newState = { ...state, error: 'Failed to fetch the greeting message', loading: false };
+        return newState;
+      });
+  },
+});
+
+export default greetingSlice.reducer;
